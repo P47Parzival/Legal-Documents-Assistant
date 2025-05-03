7755e3178bac3fb134f14b18e32c09eb70346bfc
P47Parzival <dhruvmali999@gmail.com>
2025-05-03 20:40:48 +0530

    Retrieval and store and chat working

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import GooglePalmEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains import retrieval_qa
+from langchain.chains import RetrievalQA
 from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 loader = PyMuPDFLoader("OS-Course Outline.pdf")
 docs = loader.load()
@@ -16,19 +19,19 @@ text_splitter = RecursiveCharacterTextSplitter(
 chunks = text_splitter.split_documents(docs)
 
 # Storing chunks in vecotre store 
-embeddings = GooglePalmEmbeddings()
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
 db = FAISS.from_documents(chunks, embeddings)
 db.save_local("faiss store")
 
 # Building an RAG chain
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
-qa_chain = retrieval_qa.from_chain_type(
+qa_chain = RetrievalQA.from_chain_type(
     llm = llm,
     retriever = db.as_retriever(),
     return_source_documents = True
 )
 query = "What is this documnet about?"
-response = qa_chain({"query": query})
+response = qa_chain.invoke({"query": query})
 print(response["result"])
 
 # print(chunks[0].page_content)       #this will print first chunk of text
